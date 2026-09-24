e68826d83235c530c8f00bffeb267f5ce511f46b
Alex Gravener <112940493+AlexGravener@users.noreply.github.com>
2026-09-24 09:55:10 +0100

    Create RoutineParameters.py

---
+++ RoutineParameters.py
@@ -0,0 +1,11 @@
+class RoutineParameters:
+
+  def __init__(self, user_style, duration, intensity, has_balances, has_inversions, has_meditation, injuries, focuses):
+    self.user_style = user_style
+    self.duration = duration
+    self.intensity = intensity
+    self.has_balances = has_balances
+    self.has_inversions = has_inversions
+    self.has_meditation = has_meditation
+    self.injuries = injuries
+    self.focuses = focuses
